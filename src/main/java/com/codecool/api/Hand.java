3649fd86c73b78d60be18dc0c6cec2042f849250
Tamas Kiss <sauriaster@gmail.com>
2018-02-19 14:31:55 +0100

    basic structure of hand implemented

--- src/main/java/com/codecool/api/Hand.java
+++ src/main/java/com/codecool/api/Hand.java
@@ -3,9 +3,28 @@ package com.codecool.api;
 import java.util.List;
 
 public class Hand {
-    List<Card> cardsInHand;
+    private List<Card> cardsInHand;
+    private Deck deck;
+    private GetRandom random;
 
-    public Hand() {
+    public Hand(Deck deck, GetRandom random) {
         this.cardsInHand = cardsInHand;
+        this.deck = deck;
+        this.random = random;
     }
+
+    public void draw(){
+        cardsInHand.add(deck.getRandomCards());
+
+    }
+
+    public Card play(){
+        return random.getRandomCard(cardsInHand);
+    }
+
+    public void discard(Card dicardCard){
+        cardsInHand.remove(dicardCard);
+    }
+
+
 }
