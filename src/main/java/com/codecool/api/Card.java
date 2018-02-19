4a1a0bf3a59df9ccc31905f72022a1683c45512d
Laszlo Poroszkai <laszlo.poroszkai@gmail.com>
2018-02-19 13:50:42 +0100

    cards added

---
+++ src/main/java/com/codecool/api/Card.java
@@ -0,0 +1,37 @@
+package com.codecool.api;
+
+public class Card {
+    private String name;
+    private int military;
+    private int intrique;
+    private int fame;
+    private boolean state;
+
+    public Card(String name, int military, int intrique, int fame) {
+        this.name = name;
+        this.military = military;
+        this.intrique = intrique;
+        this.fame = fame;
+        this.state = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMilitary() {
+        return military;
+    }
+
+    public int getIntrique() {
+        return intrique;
+    }
+
+    public int getFame() {
+        return fame;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+}
