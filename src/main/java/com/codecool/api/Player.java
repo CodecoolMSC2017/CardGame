e7acf11aa27e725da9b42af8e44470f85548f245
hreskonorbert <norberthresko@gmail.com>
2018-02-19 15:33:08 +0100

    Player class got a "name" field

--- src/main/java/com/codecool/api/Player.java
+++ src/main/java/com/codecool/api/Player.java
@@ -1,15 +1,17 @@
 package com.codecool.api;
 
 public class Player {
+    String name;
     Deck deck;
     Hand hand;
     Board board;
 
 
-    public Player(Deck deck, Hand hand, Board board) {
+    public Player(Deck deck, Hand hand, Board board,String name) {
         this.deck = deck;
         this.hand = hand;
         this.board = board;
+        this.name=name;
     }
 
     public Deck getDeck() {
@@ -22,6 +24,10 @@ public class Player {
 
     public Board getBoard() {
         return board;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setDeck(Deck deck) {
