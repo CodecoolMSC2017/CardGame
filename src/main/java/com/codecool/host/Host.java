49ae29a2c65dbe371c6b105b0374ba224c50af70
Tamas Kiss <sauriaster@gmail.com>
2018-02-20 14:09:31 +0100

    Client: added basic sturcture; Host: added basic sturcture; Main test calling for server and client

---
+++ src/main/java/com/codecool/host/Host.java
@@ -0,0 +1,52 @@
+package com.codecool.host;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Host{
+    private InetAddress servAddress = null;
+    private final int port = 2100;
+
+    public Host() {
+        try {
+            servAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            System.err.println("The host is unknow.");
+        }
+    }
+
+    public InetAddress getServerAddress() {
+        return servAddress;
+    }
+
+    public void setUp() {
+        try {
+            String server_IP = servAddress.getHostAddress();
+            System.out.println("Server IP address : " + server_IP);
+
+            ServerSocket server = new ServerSocket(port);
+            Socket sSocket = server.accept();
+            PrintWriter pwOut = new PrintWriter(sSocket.getOutputStream(), true);
+            InputStreamReader inp = new InputStreamReader(sSocket.getInputStream());
+            pwOut.println("ok");
+            BufferedReader bf = new BufferedReader(inp);
+
+
+            sSocket.close();
+            server.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException n){
+            System.err.println("No server ip established");
+        }
+
+    }
+}
+
